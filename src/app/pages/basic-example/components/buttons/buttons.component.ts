391c5c1d8b7e8955c9c7e87cc95d0deac3024ef5
serkuguk <sergio.kuguk@gmail.com>
2025-12-07 18:43:24 +0100

    [develop_otherLibrary] adde buton en page example

--- src/app/pages/basic-example/components/buttons/buttons.component.ts
+++ src/app/pages/basic-example/components/buttons/buttons.component.ts
@@ -4,17 +4,19 @@ import {CommonModule} from "@angular/common";
 /*import {ButtonComponent} from "@shared/components/button";
 import {LoaderComponent} from "@shared/components/loader/loader.component";*/
 import {TranslateModule} from "@ngx-translate/core";
+import {ButtonComponent} from "@shared/components/button/button.component";
 
 
 @Component({
-    selector: 'app-button',
+    selector: 'show-button',
     providers: [],
-    imports: [
-        CommonModule,
-        /*ButtonComponent,
-        LoaderComponent,*/
-        TranslateModule
-    ],
+  imports: [
+    CommonModule,
+    /*ButtonComponent,
+    LoaderComponent,*/
+    TranslateModule,
+    ButtonComponent
+  ],
     templateUrl: './buttons.component.html',
     styleUrls: ['./buttons.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
