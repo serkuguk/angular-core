49f007c63b87ce62f9916d6e421b4af1676141bd
serkuguk <sergio.kuguk@gmail.com>
2026-02-07 22:34:52 +0100

    [develop_otherLibrary] router modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import {loggedGuard, redirectLoggedInGuard} from '@pages/auth/services/auth.guard';
-import {LoginComponent} from "@pages/auth/components/login/login.component";
+import {
+  isAuthCanMach,
+  loggedGuard,
+  redirectLoggedInGuard
+} from '@pages/auth/services/auth.guard';
 
 export const routes: Routes = [
   {
@@ -10,46 +13,52 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    component: LoginComponent,
+    loadComponent: () => import('@pages/auth/components/login/login.component').then(c => c.LoginComponent),
     canActivate: [redirectLoggedInGuard]
   },
   {
     path: 'dashboard',
     loadComponent: () => import('@pages/home/home.component').then(c => c.HomeComponent),
+    canMatch: [isAuthCanMach],
     canActivate: [loggedGuard]
   },
   {
-    path: 'basic-examples/tables',
-    loadComponent: () => import('@pages/basic-example/components/tables/tables.component').then(c => c.TablesComponent),
-    canActivate: [loggedGuard]
-  },
-  {
-    path: 'basic-examples/button',
-    loadComponent: () => import('@pages/basic-example/components/buttons/buttons.component').then(c => c.ButtonsComponent),
-    canActivate: [loggedGuard]
-  },
-  {
-    path: 'basic-examples/selects',
-    loadComponent: () => import('@pages/basic-example/components/selectors/selectors.component').then(c => c.SelectorsComponent),
-    canActivate: [loggedGuard]
-  },
-  {
-    path: 'basic-examples/charts',
-    loadComponent: () => import('@pages/basic-example/components/charts/charts.component').then(c => c.ChartsComponent),
-    canActivate: [loggedGuard]
-  },
-  {
-    path: 'basic-examples/forms',
-    loadComponent: () => import('@pages/basic-example/components/forms/forms.component').then(c => c.FormsComponent),
-    canActivate: [loggedGuard]
-  },
-  {
-    path: 'basic-examples/stepper',
-    loadComponent: () => import('@pages/basic-example/components/stepper/stepper.component').then(c => c.StepperComponent),
-    canActivate: [loggedGuard]
+    path: 'basic-examples',
+    canMatch: [isAuthCanMach],
+    canActivate: [loggedGuard],
+    children: [
+      {
+        path: 'tables',
+        loadComponent: () => import('@pages/basic-example/components/tables/tables.component').then(c => c.TablesComponent),
+      },
+      {
+        path: 'button',
+        loadComponent: () => import('@pages/basic-example/components/buttons/buttons.component').then(c => c.ButtonsComponent),
+      },
+      {
+        path: 'selects',
+        loadComponent: () => import('@pages/basic-example/components/selectors/selectors.component').then(c => c.SelectorsComponent),
+      },
+      {
+        path: 'charts',
+        loadComponent: () => import('@pages/basic-example/components/charts/charts.component').then(c => c.ChartsComponent),
+      },
+      {
+        path: 'forms',
+        loadComponent: () => import('@pages/basic-example/components/forms/forms.component').then(c => c.FormsComponent),
+      },
+      {
+        path: 'stepper',
+        loadComponent: () => import('@pages/basic-example/components/stepper/stepper.component').then(c => c.StepperComponent),
+      },
+      {
+        path: 'calendar',
+        loadComponent: () => import('@pages/basic-example/components/calendar/calendar').then(c => c.CalendarExampleComponent),
+      },
+    ]
   },
   {
     path: '**',
-    loadComponent: () => import('./pages/notfound-page/notfound-page.component').then(c => c.NotfoundPageComponent)
+    loadComponent: () => import('@pages/notfound-page/notfound-page.component').then(c => c.NotfoundPageComponent)
   }
 ];
