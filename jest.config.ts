d52f5e9155909f6a65d0b17251d1856fb9238412
serkuguk <sergio.kuguk@gmail.com>
2026-05-13 20:46:19 +0200

    [develop] fixed jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'jest';
-import { createCjsPreset } from 'jest-preset-angular/presets';
+import { createCjsPreset } from 'jest-preset-angular/presets/index.js';
 
 const config: Config = {
   ...createCjsPreset(),
