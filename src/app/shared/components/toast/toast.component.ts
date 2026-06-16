f89126f4ba73933bbb057a4c968cbd78d32addac
serkuguk <sergio.kuguk@gmail.com>
2026-06-17 00:04:22 +0200

    [develop] fixed menu

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toast',
-  standalone: true,
   imports: [],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.scss'
--- src/app/shared/components/tool-tipe/tool-tipe.component.ts
+++ src/app/shared/components/tool-tipe/tool-tipe.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tool-tipe',
-  standalone: true,
   imports: [],
   templateUrl: './tool-tipe.component.html',
   styleUrl: './tool-tipe.component.scss'
